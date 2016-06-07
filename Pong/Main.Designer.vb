fa831ca751d529f1eec4309401d02938f6890803
Hanavan Kuhn <Hanavan99@gmail.com>
2016-06-07 14:04:17 -0500

    Made UI

---
+++ Pong/Main.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Screen = New System.Windows.Forms.PictureBox()
+        CType(Me.Screen, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Screen
+        '
+        Me.Screen.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Screen.Location = New System.Drawing.Point(0, 0)
+        Me.Screen.Name = "Screen"
+        Me.Screen.Size = New System.Drawing.Size(331, 211)
+        Me.Screen.TabIndex = 0
+        Me.Screen.TabStop = False
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(331, 211)
+        Me.Controls.Add(Me.Screen)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Main"
+        Me.Text = "Main"
+        CType(Me.Screen, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Screen As System.Windows.Forms.PictureBox
+End Class
---
+++ Pong/Main.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+Imports Network
+
+
+Public Class Main
+
+    Public Shared Sub Main()
+        Dim Main As New Main()
+        Main.Show()
+        Application.Run()
+    End Sub
+
+    Private Sub Main_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
+        WindowState = FormWindowState.Maximized
+        FormBorderStyle = FormBorderStyle.None
+
+    End Sub
+
+End Class
