373a0770d8adc19a250833d5eb5ea9768907ed5e
Zach Deibert <zachdeibert@gmail.com>
2016-06-07 14:06:26 -0500

    Added network framework

---
+++ Network/Pong/Network/NetworkClient.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+
+namespace Network {
+	public class NetworkClient : IDisposable {
+		public event Action<IPacket> PacketReceived;
+		public event Action<IOException> Errored;
+		private readonly TcpClient Client;
+		private readonly Stream Network;
+		private readonly Thread Thread;
+		private readonly BinaryFormatter fmt;
+		private bool Disposed;
+
+		public void Dispose() {
+			if (!Disposed) {
+				Thread.Interrupt();
+				Thread.Join();
+				Network.Close();
+				Client.Close();
+				Network.Dispose();
+			}
+		}
+
+		public void SendPacket(IPacket pkt) {
+			if (Disposed) {
+				throw new ObjectDisposedException("NetworkClient");
+			}
+			try {
+				fmt.Serialize(Network, pkt);
+			} catch (IOException ex) {
+				if (Errored == null) {
+					Console.Error.WriteLine(ex);
+				} else {
+					Errored(ex);
+				}
+			}
+		}
+
+		private void ReadingThread() {
+			if (Disposed) {
+				throw new ObjectDisposedException("NetworkClient");
+			}
+			try {
+				while (true) {
+					IPacket pkt = (IPacket) fmt.Deserialize(Network);
+					if (PacketReceived != null) {
+						PacketReceived(pkt);
+					}
+				}
+			} catch (IOException ex) {
+				if (Errored == null) {
+					Console.Error.WriteLine(ex);
+				} else {
+					Errored(ex);
+				}
+			}
+		}
+
+		public NetworkClient(IPAddress addr) {
+			Client = new TcpClient(addr);
+			Network = Client.GetStream();
+			fmt = new BinaryFormatter();
+			Thread = new Thread(ReadingThread);
+		}
+
+		~NetworkClient() {
+			Dispose();
+		}
+	}
+}
+
---
+++ Network/Pong/Network/Packets/IPacket.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Network {
+	[Serializable]
+	public interface IPacket {
+	}
+}
+
